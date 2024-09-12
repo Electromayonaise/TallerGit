a0d43ed8f51190d26cbce963bd1c6b58c9320aec
Santiago Angel <santiago.angel@u.icesi.edu.co>
2024-09-12 00:32:17 -0500

    feat(vehiculo): add potencia attribute to Vehiculo class
    Added a new attribute 'potencia' to represent the horsepower of the vehicle.
    
    Implemented getter and setter methods for the 'potencia' attribute.
    
    Validated that 'potencia' cannot be a negative value.

--- code/Vehiculo.py
+++ code/Vehiculo.py
@@ -2,13 +2,14 @@ class Vehiculo:
 
     COMBUSTIBLES_PERMITIDOS = {"Gasolina", "Diesel", "Eléctrico"}
 
-    def __init__(self, marca, modelo, año, kilometraje, estado_actual, tipo_combustible):
+    def __init__(self, marca, modelo, año, kilometraje, estado_actual, tipo_combustible, potencia):
         self._marca = marca
         self._modelo = modelo
         self._año = año
         self._kilometraje = kilometraje
         self._estado_actual = estado_actual
         self._tipo_combustible = tipo_combustible
+        self._potencia = potencia
 
     def get_marca(self):
         return self._marca
@@ -27,6 +28,9 @@ class Vehiculo:
 
     def get_tipo_combustible(self):
         return self._tipo_combustible
+    
+    def get_potencia(self):
+        return self._potencia 
 
     def set_marca(self, marca):
         self._marca = marca
@@ -45,6 +49,11 @@ class Vehiculo:
 
     def set_tipo_combustible(self, tipo_combustible):
         self._tipo_combustible = tipo_combustible
+
+    def set_potencia(self, potencia):
+        if potencia < 0:
+            raise ValueError("La potencia no puede ser negativa")
+        self._potencia = potencia
 
     def tipo_combustible(self, valor: str) -> None:
         if valor not in Vehiculo.COMBUSTIBLES_PERMITIDOS:
